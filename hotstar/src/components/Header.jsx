1455aaaa878d18d55ba595bb9362ab0a5cd0dfa9
suryakantamoharana123456 <suryakantamoharana60019@gmail.com>
2024-08-05 20:09:50 +0000

    hot

--- hotstar/src/components/Header.jsx
+++ hotstar/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import logo from '../assets/icons8-hotstar-512.png';
+import React, { useState } from 'react'
+import logo from '../assets/hotstar.webp';
 import { HiHome, 
          HiMagnifyingGlass, 
          HiStar,
@@ -11,6 +11,7 @@ import HeaderItem from './HeaderItem';
 
 
 function Header() {
+    const [toggle, setToggle] = useState(false)
     const menu=[
         {
             name:'Home',
@@ -19,7 +20,7 @@ function Header() {
             name:'Search',
             icon: HiMagnifyingGlass
         },{
-            name:'Watch List',
+            name:'WatchList',
             icon: HiPlus
         },{
             name:'Originals',
@@ -33,17 +34,30 @@ function Header() {
         }
     ]
   return (    
-    <div className='flex items-center justify-between p-5 '>
-        <div className='flex gap-8 items-center'>
-            <img src={logo} className='w-[70px] p-[10px] md:w-[115px] object-cover'/>
-            {menu.map((item)=>(
-                <HeaderItem name={item.name} Icon={item.icon}/>
-            ))}
+    <div className='flex items-center justify-between p-6 '>
+        <div className='flex gap-14 items-center'>
+            <img src={logo} className='w-[60px]  md:w-[70px] object-cover'/>
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item)=>(
+                    <HeaderItem name={item.name} Icon={item.icon}/>
+                ))}
+            </div>
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item,index)=>index<3 && (
+                    <HeaderItem name={''} Icon={item.icon}/>
+                ))}
+            </div>
+            <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+                    <HeaderItem name={''} Icon={HiDotsVertical}/>
+                    {toggle? <div className='absolute mt-3 bg-black border-[1px] border-gray-700 p-3 px-5 py-4'>
+                        {menu.map((item,index)=>index>2 &&(
+                            <HeaderItem name={item.name} Icon={item.icon}/>
+                        )) }
+                    </div>:null}
+            </div>
         </div>
-        <div>
-            <img src='https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small_2x/man-avatar-icon-free-vector.jpg'
-            className='w-[40px] h-[40px] rounded-full object-cover'/>
-        </div>
+        <img src='https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small_2x/man-avatar-icon-free-vector.jpg'
+        className='w-[40px] h-[40px] rounded-full object-cover'/>
     </div>
   )
 }
--- hotstar/src/components/HeaderItem.jsx
+++ hotstar/src/components/HeaderItem.jsx
@@ -4,7 +4,7 @@ function HeaderItem({name,Icon}) {
   return (
     <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8'>
         <Icon/>
-        <h2>{name}</h2>
+        <h2 className='hidden md:block'>{name}</h2>
     </div>
   )
 }
